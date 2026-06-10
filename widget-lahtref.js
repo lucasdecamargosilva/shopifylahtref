9ef2b90e5889ad06f8efba3a48e13d6f665992bf
Lucas de Camargo Silva <cionilucas@gmail.com>
2026-06-10 12:58:25 -0300

    feat: captura product_url (link do produto) no disparo da prova

--- widget-lahtref.js
+++ widget-lahtref.js
@@ -1479,6 +1479,7 @@ const fd = new FormData();
                     fd.append('whatsapp', '55' + phoneInput.value.replace(/\D/g, ''));
                     fd.append('phone_raw', phoneInput.value);
                     fd.append('product_name', prodName);
+                    fd.append('product_url', window.location.href);
                     fd.append('product_type', currentProduct.category);
                     fd.append('product_fit', currentProduct.fit);
                     fd.append('api_key', keyToUse);
